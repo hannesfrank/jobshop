735f4013e436266fea382bd881041a2cc85720c9
Hannes Frank <frankhannes@googlemail.com>
2016-06-24 11:33:07 +0200

    Add selection and simple crossover recombination operators to genetic search

--- jobshop/geneticSearch.py
+++ jobshop/geneticSearch.py
@@ -3,13 +3,75 @@ from .jobshop import *
 import random
 import time
 
+
+def select_best(jobs, population):
+    """Keep best half of population"""
+    population.sort()
+    # get an even number of individuals
+    # return population[:(populationSize//4) * 2]
+    # select best half
+    return population[:max(len(population) // 2, 1)]
+
+
+def select_tournament(jobs, population, tournament_size=10, p=0.9):
+    # TODO: implement tournament Selection (https://en.wikipedia.org/wiki/Tournament_selection)
+    raise(NotImplementedError("Implement tournament selection"))
+
+    nextGen = []
+
+    return nextGen
+
+
+def select_richard(jobs, population):
+    # TODO: richard selection: partition population and keep best of fraction of group
+    raise(NotImplementedError("Implement Richards selection idea"))
+
+
+def recombine_first(jobs, s1, s2):
+    """Dummy recombine."""
+    return s1
+
+
+def recombine_simpleCrossover(jobs, s1, s2):
+    """Recombine with classic crossover."""
+    cut = random.randint(0, len(s1) - 1)
+    return normalizeSchedule(jobs, s1[:cut] + s2[cut:])
+
+
+def mutate_none(jobs, s):
+    """Dummy select."""
+    pass
+
+
+def mutate_permuteSubsequence(jobs, s, max_shuffle_fraction=4):
+    """Mutate by random.shuffling a subsequence of a schedule."""
+    a, b = sorted([random.randint(0, len(s) - 1), random.randint(0, len(s) - 1)])
+
+    # The mutation should not be too large.
+    # TODO maybe just:
+    # b = random.randint(a, j*m//constant)
+    # TODO think about probabilities...
+    b = min(b, a + len(s) // max_shuffle_fraction)
+
+    shuffle(s, a, b)
+
+
+def mutate_swap(jobs, s, num_swaps=5):
+    """Mutate by swapping two instructions."""
+    for swap in range(num_swaps):
+        a = random.randint(0, len(s) - 1)
+        b = random.randint(0, len(s) - 1)
+        s[a], s[b] = s[b], s[a]
+
+
 # TODO do we need to parameterize select? (probably yes)
-def geneticSearchTemplate(jobs, mutate, recombine, populationSize=20, maxTime=None):
+def geneticSearchTemplate(jobs, recombine, mutate=mutate_none, select=select_best,
+        populationSize=100, maxTime=None):
     """
     Genetic algorithm for the jobshop scheduling problem.
     """
 
-    numGenerations = 100   # generations calculated between logging
+    numGenerations = 10   # generations calculated between logging
     solutions = []   # list of (time, schedule) with decreasing time
     best = 10000000  # TODO set initial value for max of add check in loop
 
@@ -35,11 +97,7 @@ def geneticSearchTemplate(jobs, mutate, recombine, populationSize=20, maxTime=No
 
             for g in range(numGenerations):
                 # (1) selection
-                population.sort()
-                # get an even number of individuals
-                # fittest = population[:(populationSize//4) * 2]
-                # select best half
-                fittest = population[:populationSize // 2]
+                fittest = select(jobs, population)
 
                 # (2) recombination
                 next_generation = []
@@ -96,35 +154,3 @@ def geneticSearchTemplate(jobs, mutate, recombine, populationSize=20, maxTime=No
             print("Found in {:} generations in {:.1f}s".format(totalGenerations, time.time() - t0))
 
             return solutions[-1]
-
-
-def mutate_dummy(jobs, s):
-    pass
-
-
-def recombine_dummy(jobs, s1, s2):
-    return s1
-
-
-def mutate_permuteSubsequence(jobs, s, max_shuffle_fraction=4):
-    """Mutate by random.shuffling a subsequence of a schedule."""
-    a, b = sorted([random.randint(0, len(s) - 1), random.randint(0, len(s) - 1)])
-
-    # The mutation should not be too large.
-    # TODO maybe just:
-    # b = random.randint(a, j*m//constant)
-    # TODO think about probabilities...
-    b = min(b, a + len(s) // max_shuffle_fraction)
-
-    shuffle(s, a, b)
-
-
-def mutate_swap(jobs, s):
-    """Mutate by swapping two instructions."""
-    a, b = random.randint(0, len(s) - 1)
-
-
-def recombine_simpleCrossover(jobs, s1, s2):
-    """Recombine with classic crossover."""
-    cut = random.randint(0, len(s) - 1)
-    return normalizeSchedule(jobs, s1[:cut] + s2[cut:])
